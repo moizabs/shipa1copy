04963b0ec307a87c95bd22e23f69d76075282154
moizabs <absinternational5970@gmail.com>
2025-03-28 20:21:03 +0500

    testing

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -10,12 +10,7 @@ services near you! We ensure safe, fast, and affordable auto transport services 
     <link rel="preload" as="image" href="{{ asset('/frontend/images/banner/home-underline.webp') }}" type="image/webp">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <style>
-        /* Accordion content ko by default hide rakhna */
-        .accordion-body {
-            display: none;
-        }
-    </style>
+   
 </head>
 <style>
     .card {
