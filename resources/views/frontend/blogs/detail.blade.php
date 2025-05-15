1dd8c57db08b3bc2f2c9b87e0c4c3704d5597608
moizabs <absinternational5970@gmail.com>
2025-05-16 00:15:52 +0500

    blog page changes according to seo

--- resources/views/frontend/blogs/detail.blade.php
+++ resources/views/frontend/blogs/detail.blade.php
@@ -1,10 +1,10 @@
 @extends('frontend.layouts.app')
 @section('title')
-{{-- {{ $blog->meta_title }} --}}
-{{ $blog->meta_title ? $blog->meta_title : 'Opps Title is Not Availabe' }}
+    {{-- {{ $blog->meta_title }} --}}
+    {{ $blog->meta_title ? $blog->meta_title : 'Opps Title is Not Availabe' }}
 @endsection
 @section('meta_description')
-{{ $blog->meta_description }}
+    {{ $blog->meta_description }}
 @endsection
 @section('canonical')
     <link rel="canonical" href="{{ url()->current() }}">
@@ -13,6 +13,38 @@
     <style>
         font {
             display: contents;
+        }
+
+        .name-bar2 {
+            background-color: #6a9c2f;
+            color: #ffffff;
+            font-size: 18px;
+            font-weight: 500;
+            width: 90%;
+            font-family: "DM Sans", sans-serif;
+            border-top-right-radius: 15px;
+            border-bottom-left-radius: 25px;
+
+            clip-path: polygon(0 0, 95% 0, 100% 95%, 0% 100%);
+        }
+
+        .zoom-out-image {
+            transition: transform 0.3s ease;
+        }
+
+        .zoom-out-image:hover {
+            transform: scale(1.1);
+        }
+
+        .tj-details-header2 a:hover {
+            color: #6a9c2f;
+            text-decoration: none;
+        }
+
+        .tj-details-header2 a {
+            color: #004056;
+            transition: 0.4s;
+            /* font-size: 18px; */
         }
     </style>
     <!--========== breadcrumb Start ==============-->
@@ -47,26 +79,32 @@
                         <div class="tj-blog-item-three">
                             <div class="tj-blog-image">
                                 <img loading="lazy" src="{{ asset($blog->post_image) }}" alt="Blog" />
+                                {{-- <img loading="lazy"
+                                    src="https://media.ed.edmunds-media.com/ford/f-150/2025/oem/2025_ford_f-150_crew-cab-pickup_lariat_fq_oem_1_1600.jpg"
+                                    alt="Blog" /> --}}
+                                <div class="active-text w-100">
+                                    <div class="name-bar2 w-50  py-2 pl-5">
+                                        <span><i class="fa-light fa-user"></i></span> <span
+                                            style="margin-right: 10px;">{{ $blog->user->name }}</span>
+                                        <span><i class="flaticon-calendar"></i></span>
+                                        <span>{{ \Carbon\Carbon::parse($blog->created_at)->format('M d, Y') }}</span>
+                                    </div>
+
+                                </div>
                             </div>
-                            <div class="active-text">
-                                <span class="p-2">
-                                    <span><i class="fa-light fa-user"></i></span> <span style="margin-right: 10px;">{{ $blog->user->name }}</span>
-                                    <span><i class="flaticon-calendar"></i></span> <span>{{ \Carbon\Carbon::parse($blog->created_at)->format('M d, Y') }}</span>
-                                </span>
-                                
-                            </div>
+
                             <div class="blog-content-area">
                                 <div class="blog-header">
                                     <h3>
                                         {{-- <a class="title-link" href="blog-details.html"> --}}
-                                            {{-- {{ $blog->post_name }} --}}
+                                        {{-- {{ $blog->post_name }} --}}
                                         {{-- </a> --}}
                                     </h3>
                                 </div>
                                 <div class="blog-meta">
                                 </div>
                             </div>
-                            <p>
+                            <p style="color: rgb(136, 136, 136)">
                                 {!! $blog->post_description !!}
                             </p>
                         </div>
@@ -81,14 +119,21 @@
                                     <div class="tj-auother-img">
                                         <a
                                             @if ($blog->type == 'old') href="{{ route('blog.details.noSlug', $blog->slug_name) }}" @else href="{{ route('blog.details', $blog->slug_name) }}" @endif>
-                                            <img loading="lazy" src="{{ asset($blog->post_image) }}" alt="Blog" /></a>
+                                            <img loading="lazy" class="zoom-out-image" src="{{ asset($blog->post_image) }}" alt="Blog" /></a>
+                                            {{-- <img loading="lazy" class="zoom-out-image" src="https://media.ed.edmunds-media.com/ford/f-150/2025/oem/2025_ford_f-150_crew-cab-pickup_lariat_fq_oem_1_1600.jpg" alt="Blog" /></a> --}}
                                     </div>
                                     <div class="tj-details-text">
-                                        <div class="tj-details-header">
+                                        <div class="tj-details-header2">
+                                              <span
+                                                style="color: rgb(136, 136, 136);font-size: 13px">{{ \Carbon\Carbon::parse($blog->created_at)->format('M d, Y') }}</span>
                                             <h6>
                                                 <a
-                                                    @if ($blog->type == 'old') href="{{ route('blog.details.noSlug', $blog->slug_name) }}" @else href="{{ route('blog.details', $blog->slug_name) }}" @endif>
-                                                    {{ $blog->post_name }}
+                                                    @if ($blog->type == 'old') href="{{ route('blog.details.noSlug', $blog->slug_name) }}" @else
+                                                        href="{{ route('blog.details', $blog->slug_name) }}" @endif
+                                                        title="{{ $blog->post_name }}" >
+
+                                                    {{-- {{ $blog->post_name }} --}}
+                                                    {!! Illuminate\Support\Str::limit($blog->post_name, 50, '...') !!}
                                                 </a>
                                             </h6>
                                         </div>
--- resources/views/frontend/blogs/index.blade.php
+++ resources/views/frontend/blogs/index.blade.php
@@ -1,15 +1,64 @@
 @extends('frontend.layouts.app')
 @section('title', 'Blog')
-@section('meta_description', 'Discover insightful articles, expert tips on auto shipping and transportation at SHIPA1 Blog. Stay informed and enhance your knowledge.')
+@section('meta_description',
+    'Discover insightful articles, expert tips on auto shipping and transportation at SHIPA1
+    Blog. Stay informed and enhance your knowledge.')
 @section('canonical')
     <link rel="canonical" href="{{ url()->current() }}">
 @endsection
 @section('content')
     <style>
         font {
-        display: contents;
+            display: contents;
         }
-        
+
+        .name-bar {
+            background-color: #6a9c2f;
+            color: #ffffff;
+            font-size: 18px;
+            font-weight: 500;
+            width: 90%;
+            font-family: "DM Sans", sans-serif;
+            border-top-right-radius: 15px;
+            border-bottom-left-radius: 25px;
+
+            clip-path: polygon(0 0, 95% 0, 100% 95%, 0% 100%);
+        }
+
+        .zoom-out-image {
+            transition: transform 0.3s ease;
+        }
+
+        .zoom-out-image:hover {
+            transform: scale(1.1);
+        }
+
+        .tj-details-header2 a:hover {
+            color: #6a9c2f;
+            text-decoration: none;
+        }
+
+        .tj-details-header2 a {
+            color: #004056;
+            transition: 0.4s;
+            /* font-size: 18px; */
+        }
+
+        .btn{
+          background-color: #fff;
+          color: #6a9c2f;
+          border: 1px solid #6a9c2f;
+          border-radius: 25px;
+          padding: 12px 25px;
+          transition: 1s;
+
+        }
+        .btn:hover{
+          background-color: #004056;
+          color: #fff;
+          border: 1px solid #004056;
+        }
+      
     </style>
     <!--========== breadcrumb Start ==============-->
     <section class="breadcrumb-wrapper" data-bg-image="{{ asset('frontend/images/banner/all-cover-banner.webp') }}">
@@ -42,46 +91,51 @@
                 <div class="col-lg-8">
                     <div class="page-details-wrapper">
                         @foreach ($blogs as $blog)
-                            <div class="tj-blog-item-three"  data-sal="slide-left" data-sal-duration="800" data-sal-delay="100">
+                            <div class="tj-blog-item-three" data-sal="slide-left" data-sal-duration="800"
+                                data-sal-delay="100">
                                 <div class="tj-blog-image">
                                     <a
                                         @if ($blog->type == 'old') href="{{ route('blog.details.noSlug', $blog->slug_name) }}"
                                         @else href="{{ route('blog.details', $blog->slug_name) }}" @endif>
                                         <img loading="lazy" src="{{ asset($blog->post_image) }}" alt="Blog" />
+                                        {{-- <img loading="lazy"
+                                            src="https://upload.wikimedia.org/wikipedia/commons/f/f0/2018_Ford_F-150_XLT_Crew_Cab%2C_front_11.10.19.jpg"
+                                            alt="Blog" /> --}}
                                     </a>
                                 </div>
-                                <div class="active-text">
-                                    <span class="p-2">
+                                <div class="active-text w-100">
+                                    <div class="name-bar w-50  py-2 pl-5">
                                         <span><i class="fa-light fa-user"></i></span> <span
                                             style="margin-right: 10px;">{{ $blog->user->name }}</span>
                                         <span><i class="flaticon-calendar"></i></span>
                                         <span>{{ \Carbon\Carbon::parse($blog->created_at)->format('M d, Y') }}</span>
-                                    </span>
+                                    </div>
                                 </div>
-                                <div class="tj-content-box">
-                                    <div class="blog-content-area">
+                                <div class="tj-content-box" style="position:absolute; top:96%; ">
+                                    <div class="blog-content-area ">
                                         <div class="blog-header">
-                                            <h2>
-                                                <a class="title-link fs-3"
+                                            <h3>
+                                                <a class="title-link fs-4  pt-3"
                                                     @if ($blog->type == 'old') href="{{ route('blog.details.noSlug', $blog->slug_name) }}" @else
                                                     href="{{ route('blog.details', $blog->slug_name) }}" @endif>
                                                     {{ $blog->post_name }}
                                                 </a>
-                                            </h2>
+                                            </h3>
                                         </div>
                                         <div class="blog-meta">
                                         </div>
                                     </div>
-                                    <p>
-                                        {!! Illuminate\Support\Str::limit($blog->post_short_description, 200, '...') !!}
+                                    <p style="color: rgb(136, 136, 136);">
+                                        {!! Illuminate\Support\Str::limit($blog->meta_description, 200, '...') !!}
                                     </p>
-                                    <div class="read-more">
-                                        <a
+                                   
+                                         <a class="btn my-3"
                                             @if ($blog->type == 'old') href="{{ route('blog.details.noSlug', $blog->slug_name) }}"
-                                            @else href="{{ route('blog.details', $blog->slug_name) }}" @endif>
-                                             More About {{ $blog->slug_name }} <i class="fa-light fa-arrow-right"></i>
+                                            @else href="{{ route('blog.details', $blog->slug_name) }}" @endif
+                                            > Read
+                                            More<i class="fa-light fa-arrow-right"></i>
                                         </a>
-                                    </div>
+                                    
                                 </div>
                             </div>
                         @endforeach
@@ -89,22 +143,30 @@
                 </div>
                 <div class="col-lg-4">
                     <div class="details-sidebar-inner">
-                        <div class="tj-sidebar-widget sidebar-post"   data-sal="slide-left" data-sal-duration="800" data-sal-delay="100">
+                        <div class="tj-sidebar-widget sidebar-post" data-sal="slide-left" data-sal-duration="800"
+                            data-sal-delay="100">
                             <h5 class="details_title">Recent Blogs</h5>
                             @foreach ($recent_blogs as $blog)
                                 <div class="tj-post-content">
                                     <div class="tj-auother-img">
                                         <a href="">
-                                            <img loading="lazy" src="{{ asset($blog->post_image) }}" alt="Blog Image"></a>
+                                            <img loading="lazy" class="zoom-out-image" src="{{ asset($blog->post_image) }}" alt="Blog Image"></a>
+                                            {{-- <img loading="lazy" class="zoom-out-image"
+                                                src="https://media.ed.edmunds-media.com/ford/f-150/2025/oem/2025_ford_f-150_crew-cab-pickup_lariat_fq_oem_1_1600.jpg"
+                                                alt="Blog Image"></a> --}}
                                     </div>
                                     <div class="tj-details-text">
-                                        <div class="tj-details-header">
-                                            <h6>
+                                        <div class="tj-details-header2">
+                                            <span
+                                                style="color: rgb(136, 136, 136); font-size: 13px">{{ \Carbon\Carbon::parse($blog->created_at)->format('M d, Y') }}</span>
+                                            <h6 class="fs-6">
                                                 <a
                                                     @if ($blog->type == 'old') href="{{ route('blog.details.noSlug', $blog->slug_name) }}" @else
-                                                        href="{{ route('blog.details', $blog->slug_name) }}" @endif>
+                                                        href="{{ route('blog.details', $blog->slug_name) }}" @endif
+                                                        title="{{ $blog->post_name }}" >
 
-                                                    {{ $blog->post_name }}
+                                                    {{-- {{ $blog->post_name }} --}}
+                                                    {!! Illuminate\Support\Str::limit($blog->post_name, 50, '...') !!}
                                                 </a>
                                             </h6>
                                         </div>
@@ -112,7 +174,8 @@
                                 </div>
                             @endforeach
                         </div>
-                        <div class="tj-appointment-box" data-bg-image="/assets/images/service/service-15.jpg"   data-sal="slide-left" data-sal-duration="800" data-sal-delay="100">
+                        <div class="tj-appointment-box" data-bg-image="/assets/images/service/service-15.jpg"
+                            data-sal="slide-left" data-sal-duration="800" data-sal-delay="100">
                             <div class="tj-appointment-bg" data-bg-image="/assets/images/banner/service_shape.png"></div>
                             <div class="tj-appointment-body">
                                 <div class="appointment-percent text-center">
