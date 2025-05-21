bc776bd6c202666191136d0db888433678c72ac4
moizabs <absinternational5970@gmail.com>
2025-05-21 20:44:42 +0500

    summernote adit

--- resources/views/dashboard/admin/blogs/edit.blade.php
+++ resources/views/dashboard/admin/blogs/edit.blade.php
@@ -81,4 +81,16 @@
     </div>
 
 </div>
+ <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+
+    <script>
+      $(document).ready(function () {
+        $('#summernote').summernote({
+          height: 300
+        });
+      });
+    </script>
 @endsection
