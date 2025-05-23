7e862b1eafcab1cdede6667dd5a2d211296d5395
moizabs <absinternational5970@gmail.com>
2025-05-23 22:38:32 +0500

    add some changes

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -55,7 +55,9 @@
         <div class=row>
             <div class="col-xl-4 col-lg-3 col-md-6 col-sm-6 ">
                 <div class="footer-widget footer1_col_1 footer-content-info " >
-                    <a href="{{ route('welcome') }}"> <img src="{{ asset('/frontend/images/logo/logo-white-2.webp') }}"
+                    {{-- <a href="{{ route('welcome') }}"> <img src="{{ asset('/frontend/images/logo/logo-white-2.webp') }}"
+                            loading=lazy alt=Logo></a> --}}
+                    <a href="{{ route('welcome') }}"> <img src="{{ asset('/frontend/images/logo/footer-new-logo.png') }}"
                             loading=lazy alt=Logo></a>
                     <p>
                         Ship A1 Transport is one of the top logistic and shipping companies, with a vast variety of
