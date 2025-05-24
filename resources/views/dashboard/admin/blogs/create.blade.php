295cabd704f3487fc44c61f818ce8d24a5f76dbe
moizabs <absinternational5970@gmail.com>
2025-05-24 18:30:15 +0500

    changes

--- resources/views/dashboard/admin/blogs/create.blade.php
+++ resources/views/dashboard/admin/blogs/create.blade.php
@@ -122,7 +122,14 @@
     <script>
       $(document).ready(function () {
         $('#summernote').summernote({
-          height: 300
+          toolbar: [
+              ['style', ['bold', 'italic', 'underline', 'clear']],
+              ['font', ['strikethrough', 'superscript', 'subscript']],
+              ['fontsize', ['fontsize']],
+              ['color', ['color']],
+              ['para', ['ul', 'ol', 'paragraph']],
+              ['height', ['height']]
+            ]
         });
       });
     </script>
