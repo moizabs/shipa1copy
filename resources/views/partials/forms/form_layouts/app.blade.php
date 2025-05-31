df98e8e401606086f6f49e4dee4614c9c519c834
moizabs <absinternational5970@gmail.com>
2025-05-31 18:52:25 +0500

    changes 6.0

--- resources/views/partials/forms/form_layouts/app.blade.php
+++ resources/views/partials/forms/form_layouts/app.blade.php
@@ -1,32 +1,32 @@
 <link rel="apple-touch-icon" href="{{ asset('frontend/images/logo/favicon.webp') }}" />
 <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/images/logo/favicon.webp') }}" />
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/meanmenu.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/sal.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/magnific-popup.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/swiper.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/icons.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/odometer.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/nice-select.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}" />
-<link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/bootstrap.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/meanmenu.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/sal.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/magnific-popup.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/swiper.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/owl.carousel.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/icons.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/odometer.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/nice-select.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/animate.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/style.css') }}" />
+<link rel="stylesheet" href="{{ asset('public/frontend/css/responsive.css') }}" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 @yield('forms_content')
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="{{ asset('frontend/js/modernizr-2.8.3.min.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
-<script src="{{ asset('frontend/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('frontend/js/meanmenu.js') }}"></script>
-<script src="{{ asset('frontend/js/imagesloaded.pkgd.min.js') }}"></script>
-<script src="{{ asset('frontend/js/isotope.pkgd.min.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery.magnific-popup.min.js') }}"></script>
-<script src="{{ asset('frontend/js/swiper.min.js') }}"></script>
-<script src="{{ asset('frontend/js/owl.carousel.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery.appear.min.js') }}"></script>
-<script src="{{ asset('frontend/js/odometer.min.js') }}"></script>
-<script src="{{ asset('frontend/js/sal.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery.nice-select.min.js') }}"></script>
-<script src="{{ asset('frontend/js/main.js') }}"></script>
+<script src="{{ asset('public/frontend/js/modernizr-2.8.3.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/jquery.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/meanmenu.js') }}"></script>
+<script src="{{ asset('public/frontend/js/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/swiper.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/owl.carousel.js') }}"></script>
+<script src="{{ asset('public/frontend/js/jquery.appear.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/odometer.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/sal.js') }}"></script>
+<script src="{{ asset('public/frontend/js/jquery.nice-select.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/main.js') }}"></script>
