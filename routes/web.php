a4f64441cbb6f33be2e827d182ec1749d89c612e
internationalabs <internationalabs100@gmail.com>
2025-06-02 21:18:46 +0500

    changes 8.6

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,9 @@ use Illuminate\Support\Facades\Request;
 // })->where('path', '.*');
 
 
-// Route::get('/public/{any}', function ($any) {
-//     return redirect('/' . $any, 301);
-// })->where('any', '.*');
+Route::get('/we/{any}', function ($any) {
+    return redirect('/' . $any, 301);
+})->where('any', '.*');
 
 Route::get('/robots.txt', function () {
     return response()->view('robots')->header('Content-Type', 'text/plain');
